57f592962d8cdf230e02921cb8b9f680494a21cf
sbobopst <dbgian23@gmail.com>
2026-05-23 14:54:35 +0200

    feat: add ansible command availability check with shutil.which

--- ansibleip.py
+++ ansibleip.py
@@ -1,9 +1,11 @@
 import subprocess 
+import shutil
 
 inv_path = "~/ansible-infra-lab/inventories/lab/hosts.yml"
 def ansibleping():
-
-    command = ['ansible', '-i', inv_path, 'all', '-m', 'ping']
-
-    return subprocess.call(command) == 0
-
+	if(shutil.which('ansible')!=None):
+		command = ['ansible', '-i', inv_path, 'all', '-m', 'ping']
+		return subprocess.call(command) == 0
+	else:
+		print("Ansible not found")
+		return False
